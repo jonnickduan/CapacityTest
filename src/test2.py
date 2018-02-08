f3cd826967d31b4b5184f0d2e9438e436e8c6193
Jonnick Duan <jonnick.duan@outlook.com>
2018-02-08 16:14:22 +0800

    creat

--- src/test2.py
+++ src/test2.py
@@ -1,23 +1,11 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
-import pp
 import sys
 import time
+import pp
 
-def test_login(phone_number):
-    from pyhessian.client import HessianProxy
-    service = HessianProxy("https://dev.188yd.com/services/AccountService")
-    device_info_dto = {
-        'clientType':1,
-        'clientVer':'1.0',
-        'deviceName':'Lao Duan\'s iphone',
-        'deviceId':'xxx001',
-        'lastLat':0.00,
-        'lastLng':0.00,
-        'relativeId1':'channel',
-        'relativeId2':'user_id'
-    }
-    service.sellerLogin(None, phone_number, "000000", device_info_dto)
+
+
 
 ppservers = ()
 if len(sys.argv) > 1:
@@ -29,12 +17,10 @@ else:
 print("Starting pp with", job_server.get_ncpus(), "workers")
 
 start_time = time.time()
-inputs = ('13910625431','15727326906')
+inputs = ('15727326906','13910600000')
 jobs = [(input, job_server.submit(test_login,(input,))) for input in inputs]
 for input, job in jobs:
     print("test_login", input, "is", job())
 
 print("Time elapsed: ", time.time() - start_time, "s")
 job_server.print_stats()
-
-
